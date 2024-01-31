25b450a7b82b7a9a093b5b4235f6314f3966d142
PlusLius <408709581@qq.com>
2024-01-31 09:10:13 +0800

    add: React组件命名空间申明合并问题的一些解决方法

---
+++ src/07-types-deep-dive/60-add-new-global-element.solution.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "something-solution": {
+        id: string;
+      };
+    }
+  }
+}
+
+<>
+  <something-solution id="123" />
+
+  {/* @ts-expect-error */}
+  <something-solution />
+
+  {/* @ts-expect-error */}
+  <something-solution id={123} />
+</>;
